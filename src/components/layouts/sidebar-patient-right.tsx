bd1312c6d6ff8db16c0687684431b58fba0d3938
joaopaulo-D <devjoaopaulo2003@gmail.com>
2023-12-06 21:20:56 -0300

    add: add loading response api classify

--- src/components/layouts/sidebar-patient-right.tsx
+++ src/components/layouts/sidebar-patient-right.tsx
@@ -17,9 +17,11 @@ export default function SideBarRight({ image, medicine, typeMedicine }: IProps) 
   console.log(typeMedicine)
 
   const [classify, setClassify] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(false)
 
   const classifyImage = async () => {
     try {
+      setLoading(true)
       const formData = new FormData()
       formData.append('image', image)
 
@@ -32,7 +34,9 @@ export default function SideBarRight({ image, medicine, typeMedicine }: IProps) 
       if (response.data) {
         setClassify(response?.data.classifcation)
       }
+      setLoading(false)
     } catch (error) {
+      setLoading(false)
       console.log(error)
     }
   }
@@ -45,71 +49,79 @@ export default function SideBarRight({ image, medicine, typeMedicine }: IProps) 
     <aside
       className="bg-[#333232] text-white fixed top-0 right-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
     >
-      <div>
-        <div className="flex p-4">
-          <h1 className="text-sm font-bold uppercase">
-            Patologias - Analisadas
-          </h1>
+      {loading ? (
+        <div className="w-full flex justify-center items-center">
+          <span className="text-white text-center">Analizando exame ...</span>
         </div>
-        <div className="p-4">
-          <div className="flex items-center mb-4">
-            <input
-              id="disabled-radio-1"
-              checked={classify?.classe == 0 ? true : false}
-              type="radio"
-              value=""
-              name="disabled-radio"
-              className="w-4 h-4 text-green-600 bg-gray-100 border-gray-30"
-            />
-            <label htmlFor="disabled-radio-1" className="ms-2 text-sm font-medium text-white">AVC hemorrágico</label>
-          </div>
-          <div className="flex items-center">
-            <input
-              id="disabled-radio-2"
-              checked={classify?.classe == 1 ? true : false}
-              type="radio"
-              value=""
-              name="disabled-radio"
-              className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300"
-            />
-            <label htmlFor="disabled-radio-2" className="ms-2 text-sm font-medium text-white">AVC isquêmico</label>
-          </div>
-        </div>
-      </div>
-      <Separator />
-      <div className="w-full p-4 space-y-2">
-        <h1 className="uppercase text-sm font-bold">Alertas</h1>
-        <div className="w-full">
-          <span className="bg-red-500 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded">
-            {classify?.classe == 0 ? "AVC hemorrágico" : "AVC isquêmico"}
-          </span>
-        </div>
-      </div>
-      <Separator />
-      <div className="w-full p-4 space-y-2">
-        <h1 className="uppercase text-sm font-bold">Medicamentos</h1>
-        <div className="w-full">
-
-          {typeMedicine?.map((tags: any, i: any) => (
+      ) : (
+        <>
+          <div>
+            <div className="flex p-4">
+              <h1 className="text-sm font-bold uppercase">
+                Patologias - Analisadas
+              </h1>
+            </div >
+            <div className="p-4">
+              <div className="flex items-center mb-4">
+                <input
+                  id="disabled-radio-1"
+                  checked={classify?.classe == 0 ? true : false}
+                  type="radio"
+                  value=""
+                  name="disabled-radio"
+                  className="w-4 h-4 text-green-600 bg-gray-100 border-gray-30"
+                />
+                <label htmlFor="disabled-radio-1" className="ms-2 text-sm font-medium text-white">AVC hemorrágico</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="disabled-radio-2"
+                  checked={classify?.classe == 1 ? true : false}
+                  type="radio"
+                  value=""
+                  name="disabled-radio"
+                  className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300"
+                />
+                <label htmlFor="disabled-radio-2" className="ms-2 text-sm font-medium text-white">AVC isquêmico</label>
+              </div>
+            </div>
+          </div >
+          <Separator />
+          <div className="w-full p-4 space-y-2">
+            <h1 className="uppercase text-sm font-bold">Alertas</h1>
             <div className="w-full">
-              <span className={twMerge(
-                "text-black text-sm font-medium me-1 px-2.5 py-0.5 rounded",
-                "bg-white"
-              )}>
-                {tags}
+              <span className="bg-red-500 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded">
+                {classify?.classe == 0 ? "AVC hemorrágico" : "AVC isquêmico"}
               </span>
             </div>
-          ))}
-          {!typeMedicine ? (
-            <span className={twMerge(
-              "text-black text-sm font-medium px-2.5 py-0.5 rounded",
-              "bg-white"
-            )}>
-              Sem uso de medicamentos
-            </span>
-          ) : null}
-        </div>
-      </div>
+          </div>
+          <Separator />
+          <div className="w-full p-4 space-y-2">
+            <h1 className="uppercase text-sm font-bold">Medicamentos</h1>
+            <div className="w-full">
+
+              {typeMedicine?.map((tags: any, i: any) => (
+                <div className="w-full">
+                  <span className={twMerge(
+                    "text-black text-sm font-medium me-1 px-2.5 py-0.5 rounded",
+                    "bg-white"
+                  )}>
+                    {tags}
+                  </span>
+                </div>
+              ))}
+              {!typeMedicine ? (
+                <span className={twMerge(
+                  "text-black text-sm font-medium px-2.5 py-0.5 rounded",
+                  "bg-white"
+                )}>
+                  Sem uso de medicamentos
+                </span>
+              ) : null}
+            </div>
+          </div>
+        </>
+      )}
     </aside >
   )
 }
